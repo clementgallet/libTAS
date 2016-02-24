8d2702872c2613a9a777e2021d084260a021aaf9
Clément Gallet <clement.gallet@ens-lyon.org>
2016-02-24 15:16:49 +0100

    Add some colors to console log

--- src/libTAS/logging.c
+++ src/libTAS/logging.c
@@ -6,7 +6,13 @@ void debuglog(LogCategoryFlag lcf, const char* fmt, ...)
     if ( (lcf & tasflags.includeFlags) && !(lcf & tasflags.excludeFlags) ) {
         char str[4096];
         memset(str, '\0', sizeof(str));
-        strcpy(str, "[libTAS] ");
+        if (lcf & LCF_ERROR)
+            /* Write the text in red */
+            strcat(str, ANSI_COLOR_RED);
+        else if ((lcf & LCF_FRAME) || (lcf & LCF_FREQUENT))
+            /* Write the text in gray */
+            strcat(str, ANSI_COLOR_GRAY);
+        strcat(str, "[libTAS] ");
         size_t str_len = strlen(str);
 
         va_list args;
@@ -14,6 +20,9 @@ void debuglog(LogCategoryFlag lcf, const char* fmt, ...)
         vsnprintf(str + str_len, 4096 - str_len - 1, fmt, args);
         va_end(args);
 
+        if ((lcf & LCF_ERROR) || (lcf & LCF_FRAME) || (lcf & LCF_FREQUENT))
+            /* Reset color change */
+            strcat(str, ANSI_COLOR_RESET);
         strcat(str, "\n");
         fprintf(stderr, str);
     }
--- src/libTAS/logging.h
+++ src/libTAS/logging.h
@@ -8,6 +8,18 @@
 #include "../shared/tasflags.h"
 #include "../shared/lcf.h"
 
+/* Color printing
+ * Taken from http://stackoverflow.com/questions/3219393/stdlib-and-colored-output-in-c
+ */
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_GRAY    "\x1b[37m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void debuglog(LogCategoryFlag lcf, const char* msg, ...);
 
 #endif // LOGCATEGORY_H_INCL
