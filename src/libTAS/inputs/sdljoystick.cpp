80b1f79867d6855f5067611d752e33017e084067
Clément Gallet <clement.gallet@ens-lyon.org>
2016-04-07 15:13:20 +0200

    Implement most of SDL_Joystick API

--- src/libTAS/inputs/sdljoystick.cpp
+++ src/libTAS/inputs/sdljoystick.cpp
@@ -32,42 +32,225 @@
     return tasflags.numControllers;
 }
 
-/* Xbox 360 GUID */
-SDL_JoystickGUID xinputGUID = {{3,0,0,0,94,4,0,0,142,2,0,0,20,1,0,0}};
+const char* joyname = "Microsoft X-Box 360 pad";
 
-SDL_JoystickGUID SDL_JoystickGetGUID(SDL_Joystick * joystick)
+/* Override */ const char *SDL_JoystickNameForIndex(int device_index)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
-	return xinputGUID;
+    if (device_index < tasflags.numControllers)
+        return joyname;
+    return NULL;
 }
 
-const char* SDL_JoystickName(SDL_Joystick* joystick)
+/* Override */ const char* SDL_JoystickName(SDL_Joystick* joystick)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
     return "Microsoft X-Box 360 pad";
 }
 
-int SDL_JoystickNumAxes(SDL_Joystick* joystick)
+#define MAX_SDLJOYS 4
+static SDL_Joystick joyids[MAX_SDLJOYS] = {-1, -1, -1, -1};
+
+/* Helper functions */
+static bool isIdValid(SDL_Joystick* joy)
+{
+    if (joy == NULL)
+        return false;
+    if ((*joy < 0) || (*joy >= MAX_SDLJOYS) || (*joy >= tasflags.numControllers))
+        return false;
+    return true;
+}
+
+static bool isIdValidOpen(SDL_Joystick* joy)
+{
+    if (!isIdValid(joy))
+        return false;
+    if (joyids[*joy] == -1)
+        return false;
+    return true;
+}
+
+
+/* Override */ SDL_Joystick *SDL_JoystickOpen(int device_index)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with joy ", device_index);
+    if ((device_index < 0) || (device_index >= MAX_SDLJOYS))
+        return NULL;
+    if (device_index >= tasflags.numControllers)
+        return NULL;
+    if (joyids[device_index] != -1)
+        /* Device already opened */
+        return NULL;
+
+    /* Opening the joystick device */
+    joyids[device_index] = device_index;
+    return &joyids[device_index];
+}
+
+/* Override */ SDL_Joystick *SDL_JoystickFromInstanceID(SDL_JoystickID joyid)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with joy id ", joyid);
+
+    if ((joyid < 0) || (joyid >= MAX_SDLJOYS))
+        return NULL;
+    if (joyid >= tasflags.numControllers)
+        return NULL;
+    if (joyids[joyid] == -1)
+        /* Device not opened */
+        return NULL;
+    
+    return &joyids[joyid];
+}
+
+/* Xbox 360 GUID */
+SDL_JoystickGUID xinputGUID = {{3,0,0,0,94,4,0,0,142,2,0,0,20,1,0,0}};
+SDL_JoystickGUID nullGUID   = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+/* Override */ SDL_JoystickGUID SDL_JoystickGetDeviceGUID(int device_index)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with device ", device_index);
+    if (device_index >= tasflags.numControllers)
+	    return nullGUID;
+
+    return xinputGUID;
+}
+
+/* Override */ SDL_JoystickGUID SDL_JoystickGetGUID(SDL_Joystick * joystick)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
+    if (!isIdValid(joystick))
+        return nullGUID;
+
+    return xinputGUID;
+}
+
+/* Override */ SDL_bool SDL_JoystickGetAttached(SDL_Joystick * joystick)
+{
+    if (!isIdValidOpen(joystick))
+        return SDL_FALSE;
+
+    return SDL_TRUE;
+}
+
+/* Override */ SDL_JoystickID SDL_JoystickInstanceID(SDL_Joystick * joystick)
+{
+    if (!isIdValidOpen(joystick))
+        return -1;
+
+    return (SDL_JoystickID) joyids[*joystick];
+
+}
+
+/* Override */ int SDL_JoystickNumAxes(SDL_Joystick* joystick)
+{
+    DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
+    if (!isIdValid(joystick))
+        return 0;
     return 6;
 }
 
-int SDL_JoystickNumBalls(SDL_Joystick* joystick)
+/* Override */ int SDL_JoystickNumBalls(SDL_Joystick* joystick)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
+    if (!isIdValid(joystick))
+        return 0;
     return 0;
 }
 
-int SDL_JoystickNumButtons(SDL_Joystick* joystick)
+/* Override */ int SDL_JoystickNumButtons(SDL_Joystick* joystick)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
+    if (!isIdValid(joystick))
+        return 0;
     return 11;
 }
 
-int SDL_JoystickNumHats(SDL_Joystick* joystick)
+/* Override */ int SDL_JoystickNumHats(SDL_Joystick* joystick)
 {
     DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK);
+    if (!isIdValid(joystick))
+        return 0;
     return 1;
+}
+
+/* Override */ void SDL_JoystickUpdate(void)
+{
+    DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK | LCF_TODO);
+}
+
+/* Override */ int SDL_JoystickEventState(int state)
+{
+    DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK | LCF_TODO);
+    return 0;
+}
+
+/* Override */ Sint16 SDL_JoystickGetAxis(SDL_Joystick * joystick, int axis)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with axis ", axis);
+
+    if (!isIdValidOpen(joystick))
+        return 0;
+
+    if (axis >= 6)
+        return 0;
+
+    return ai.controller_axes[*joystick][axis];
+}
+
+/* Override */ Uint8 SDL_JoystickGetHat(SDL_Joystick * joystick, int hat)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with hat ", hat);
+
+    if (!isIdValidOpen(joystick))
+        return 0;
+
+    if (hat > 0)
+        return 0;
+
+    Uint8 hatState = SDL_HAT_CENTERED;
+    if (ai.controller_buttons[*joystick] & (1 << SDL_CONTROLLER_BUTTON_DPAD_UP))
+        hatState |= SDL_HAT_UP;
+    if (ai.controller_buttons[*joystick] & (1 << SDL_CONTROLLER_BUTTON_DPAD_DOWN))
+        hatState |= SDL_HAT_DOWN;
+    if (ai.controller_buttons[*joystick] & (1 << SDL_CONTROLLER_BUTTON_DPAD_LEFT))
+        hatState |= SDL_HAT_LEFT;
+    if (ai.controller_buttons[*joystick] & (1 << SDL_CONTROLLER_BUTTON_DPAD_RIGHT))
+        hatState |= SDL_HAT_RIGHT;
+
+    return hatState;
+}
+
+/* Override */ int SDL_JoystickGetBall(SDL_Joystick * joystick, int ball, int *dx, int *dy)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with ball ", ball);
+    return 0;
+}
+
+/* Override */ Uint8 SDL_JoystickGetButton(SDL_Joystick * joystick, int button)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with button ", button);
+
+    if (!isIdValidOpen(joystick))
+        return 0;
+
+    if (button >= 11)
+        return 0;
+
+    return (ai.controller_buttons[*joystick] >> button) & 0x1;
+}
+
+/* Override */ void SDL_JoystickClose(SDL_Joystick * joystick)
+{
+    debuglog(LCF_SDL | LCF_JOYSTICK, __func__, " call with joy ", *joystick);
+    if (!isIdValidOpen(joystick))
+        return;
+
+    joyids[*joystick] = -1;
+}
+
+/* Override */ SDL_JoystickPowerLevel SDL_JoystickCurrentPowerLevel(SDL_Joystick * joystick)
+{
+    DEBUGLOGCALL(LCF_SDL | LCF_JOYSTICK | LCF_TODO);
+	return SDL_JOYSTICK_POWER_WIRED;
 }
 
--- src/libTAS/inputs/sdljoystick.h
+++ src/libTAS/inputs/sdljoystick.h
@@ -36,6 +36,34 @@ typedef struct {
 OVERRIDE int SDL_NumJoysticks(void);
 
 /**
+ *  Get the implementation dependent name of a joystick.
+ *  This can be called before any joysticks are opened.
+ *  If no name can be found, this function returns NULL.
+ */
+OVERRIDE const char *SDL_JoystickNameForIndex(int device_index);
+
+/**
+ *  Open a joystick for use.
+ *  The index passed as an argument refers to the N'th joystick on the system.
+ *  This index is not the value which will identify this joystick in future
+ *  joystick events.  The joystick's instance id (::SDL_JoystickID) will be used
+ *  there instead.
+ *
+ *  \return A joystick identifier, or NULL if an error occurred.
+ */
+OVERRIDE SDL_Joystick *SDL_JoystickOpen(int device_index);
+
+/**
+ * Return the SDL_Joystick associated with an instance id.
+ */
+OVERRIDE SDL_Joystick *SDL_JoystickFromInstanceID(SDL_JoystickID joyid);
+
+/**
+ *  Return the GUID for the joystick at this index
+ */
+OVERRIDE SDL_JoystickGUID SDL_JoystickGetDeviceGUID(int device_index);
+
+/**
  *  Return the GUID for this opened joystick
  */
 OVERRIDE SDL_JoystickGUID SDL_JoystickGetGUID(SDL_Joystick * joystick);
@@ -45,6 +73,16 @@ OVERRIDE SDL_JoystickGUID SDL_JoystickGetGUID(SDL_Joystick * joystick);
  *  If no name can be found, this function returns NULL.
  */
 OVERRIDE const char *SDL_JoystickName(SDL_Joystick * joystick);
+
+/**
+ *  Returns SDL_TRUE if the joystick has been opened and currently connected, or SDL_FALSE if it has not.
+ */
+OVERRIDE SDL_bool SDL_JoystickGetAttached(SDL_Joystick * joystick);
+
+/**
+ *  Get the instance ID of an opened joystick or -1 if the joystick is invalid.
+ */
+OVERRIDE SDL_JoystickID SDL_JoystickInstanceID(SDL_Joystick * joystick);
 
 /**
  *  Get the number of general axis controls on a joystick.
@@ -68,6 +106,105 @@ OVERRIDE int SDL_JoystickNumHats(SDL_Joystick * joystick);
  *  Get the number of buttons on a joystick.
  */
 OVERRIDE int SDL_JoystickNumButtons(SDL_Joystick * joystick);
+
+/**
+ *  Update the current state of the open joysticks.
+ *
+ *  This is called automatically by the event loop if any joystick
+ *  events are enabled.
+ */
+OVERRIDE void SDL_JoystickUpdate(void);
+
+/**
+ *  Enable/disable joystick event polling.
+ *
+ *  If joystick events are disabled, you must call SDL_JoystickUpdate()
+ *  yourself and check the state of the joystick when you want joystick
+ *  information.
+ *
+ *  The state can be one of ::SDL_QUERY, ::SDL_ENABLE or ::SDL_IGNORE.
+ */
+OVERRIDE int SDL_JoystickEventState(int state);
+
+/**
+ *  Get the current state of an axis control on a joystick.
+ *
+ *  The state is a value ranging from -32768 to 32767.
+ *
+ *  The axis indices start at index 0.
+ */
+OVERRIDE Sint16 SDL_JoystickGetAxis(SDL_Joystick * joystick, int axis);
+
+/**
+ *  \name Hat positions
+ */
+/* @{ */
+#define SDL_HAT_CENTERED    0x00
+#define SDL_HAT_UP          0x01
+#define SDL_HAT_RIGHT       0x02
+#define SDL_HAT_DOWN        0x04
+#define SDL_HAT_LEFT        0x08
+#define SDL_HAT_RIGHTUP     (SDL_HAT_RIGHT|SDL_HAT_UP)
+#define SDL_HAT_RIGHTDOWN   (SDL_HAT_RIGHT|SDL_HAT_DOWN)
+#define SDL_HAT_LEFTUP      (SDL_HAT_LEFT|SDL_HAT_UP)
+#define SDL_HAT_LEFTDOWN    (SDL_HAT_LEFT|SDL_HAT_DOWN)
+/* @} */
+
+/**
+ *  Get the current state of a POV hat on a joystick.
+ *
+ *  The hat indices start at index 0.
+ *
+ *  \return The return value is one of the following positions:
+ *           - ::SDL_HAT_CENTERED
+ *           - ::SDL_HAT_UP
+ *           - ::SDL_HAT_RIGHT
+ *           - ::SDL_HAT_DOWN
+ *           - ::SDL_HAT_LEFT
+ *           - ::SDL_HAT_RIGHTUP
+ *           - ::SDL_HAT_RIGHTDOWN
+ *           - ::SDL_HAT_LEFTUP
+ *           - ::SDL_HAT_LEFTDOWN
+ */
+OVERRIDE Uint8 SDL_JoystickGetHat(SDL_Joystick * joystick, int hat);
+
+/**
+ *  Get the ball axis change since the last poll.
+ *
+ *  \return 0, or -1 if you passed it invalid parameters.
+ *
+ *  The ball indices start at index 0.
+ */
+OVERRIDE int SDL_JoystickGetBall(SDL_Joystick * joystick, int ball, int *dx, int *dy);
+
+/**
+ *  Get the current state of a button on a joystick.
+ *
+ *  The button indices start at index 0.
+ */
+OVERRIDE Uint8 SDL_JoystickGetButton(SDL_Joystick * joystick, int button);
+
+/**
+ *  Close a joystick previously opened with SDL_JoystickOpen().
+ */
+OVERRIDE void SDL_JoystickClose(SDL_Joystick * joystick);
+
+
+typedef enum
+{
+    SDL_JOYSTICK_POWER_UNKNOWN = -1,
+    SDL_JOYSTICK_POWER_EMPTY,
+    SDL_JOYSTICK_POWER_LOW,
+    SDL_JOYSTICK_POWER_MEDIUM,
+    SDL_JOYSTICK_POWER_FULL,
+    SDL_JOYSTICK_POWER_WIRED,
+    SDL_JOYSTICK_POWER_MAX
+} SDL_JoystickPowerLevel;
+
+/**
+ *  Return the battery level of this joystick
+ */
+OVERRIDE SDL_JoystickPowerLevel SDL_JoystickCurrentPowerLevel(SDL_Joystick * joystick);
 
 #endif
 
