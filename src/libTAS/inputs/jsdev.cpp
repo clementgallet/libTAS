777a40b7f8caa5fb539780b4251d10fde23af224
clementgallet <clement.gallet@ens-lyon.org>
2017-09-22 11:32:01 +0200

    Correctly insert events in the fake /dev/input/jsX file

--- src/libTAS/inputs/jsdev.cpp
+++ src/libTAS/inputs/jsdev.cpp
@@ -63,12 +63,12 @@ int open_jsdev(const char* source, int flags)
     for (int button = 0; button < 11; button++) {
         ev.type = JS_EVENT_BUTTON | JS_EVENT_INIT;
         ev.number = button;
-        write(fd, &ev, sizeof(ev));
+        write_jsdev(ev, jsnum);
     }
-    for (int axis = 0; axis < 11; axis++) {
+    for (int axis = 0; axis < 8; axis++) {
         ev.type = JS_EVENT_AXIS | JS_EVENT_INIT;
         ev.number = axis;
-        write(fd, &ev, sizeof(ev));
+        write_jsdev(ev, jsnum);
     }
 
     return fd;
@@ -76,9 +76,71 @@ int open_jsdev(const char* source, int flags)
 
 void write_jsdev(struct js_event ev, int jsnum)
 {
-    if (jsdevfds[jsnum] != 0) {
-        write(jsdevfds[jsnum], &ev, sizeof(ev));
+    int fd = jsdevfds[jsnum];
+
+    if (fd == 0)
+        return;
+
+    /* We must simulate a queue using our memfd structure (basically a file).
+     * Of course, we must not grow our file too much.
+     * One thing that helps us is that we can insert events of the same frame
+     * at any order. To acheive that, we insert the event backward from
+     * the file position if the next event is from the same frame (or eof).
+     *
+     *    <---------------------|~~~~~~~~~~~~~~~~~~~|------------------------>
+     *            read         new       new       old         unread
+     *                       position   event    position
+     *
+     * For any other case, we append at the end.
+     *
+     *    <|---------------------------------------------------|+++++++++++++>
+     *  old/new                 unread                            new event
+     *  position
+     *
+     */
+
+    off_t curpos = lseek(fd, 0, SEEK_CUR);
+    MYASSERT(curpos >= 0)
+
+    off_t jsev_size = static_cast<off_t>(sizeof(struct js_event));
+
+    if (curpos > 0) {
+        MYASSERT(curpos >= jsev_size)
+
+        /* Check if we have an event stored after this one */
+        off_t lastpos = lseek(fd, 0, SEEK_END);
+        MYASSERT(lastpos >= 0)
+
+        if (lastpos == curpos) {
+            /* EOF, insert this event backward */
+            lseek(fd, lastpos - jsev_size, SEEK_SET);
+            write(jsdevfds[jsnum], &ev, jsev_size);
+            lseek(fd, lastpos - jsev_size, SEEK_SET);
+            debuglog(LCF_JOYSTICK | LCF_EVENTS, "    Wrote jsdev event at offset ", lastpos - jsev_size);
+            return;
+        }
+
+        /* Read the next event and check if time match */
+        MYASSERT((lastpos - curpos) >= jsev_size)
+        struct js_event next_ev;
+        lseek(fd, curpos, SEEK_SET);
+        read(fd, &next_ev, sizeof(next_ev));
+
+        if (next_ev.time == ev.time) {
+            /* Events are from the same frame, insert this event backward */
+            lseek(fd, curpos - jsev_size, SEEK_SET);
+            write(jsdevfds[jsnum], &ev, jsev_size);
+            lseek(fd, curpos - jsev_size, SEEK_SET);
+            debuglog(LCF_JOYSTICK | LCF_EVENTS, "    Wrote jsdev event at offset ", curpos - jsev_size);
+            return;
+        }
     }
+
+    /* In any other case, insert the event at the end of the file */
+    off_t writepos = lseek(fd, 0, SEEK_END);
+    write(jsdevfds[jsnum], &ev, jsev_size);
+    debuglog(LCF_JOYSTICK | LCF_EVENTS, "    Wrote jsdev event at offset ", writepos);
+    lseek(fd, curpos, SEEK_SET);
 }
 
 void close_jsdev(int fd)
