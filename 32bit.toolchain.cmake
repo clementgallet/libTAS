2f139d1909fb1516257ccf2478c765b3e7d59e61
nlburgin <30382228+nlburgin@users.noreply.github.com>
2019-04-01 23:50:21 -0400

    Using an actual cross-compiler seems more reliable
    And it seems that CMake will take notice and set up the rest of it for you automatically, if you just tell it to use a cross-compiler instead of a regular one.

--- 32bit.toolchain.cmake
+++ 32bit.toolchain.cmake
@@ -1,8 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER gcc)
+set(CMAKE_C_COMPILER i686-linux-gnu-gcc)
 set(CMAKE_C_FLAGS -m32)
-set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_CXX_COMPILER i686-linux-gnu-g++)
 set(CMAKE_CXX_FLAGS -m32)
 set(CMAKE_SYSTEM_PROCESSOR "i686")
-
