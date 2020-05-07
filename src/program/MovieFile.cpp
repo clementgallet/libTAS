e0431b899af97b7646fd0db3b2341cca8292194b
Clément Gallet <clement.gallet@ens-lyon.org>
2020-05-07 14:24:50 +0200

    Fixed mouse regex (#336)

--- src/program/MovieFile.cpp
+++ src/program/MovieFile.cpp
@@ -33,7 +33,7 @@
 MovieFile::MovieFile(Context* c) : modifiedSinceLastSave(false), modifiedSinceLastAutoSave(false), modifiedSinceLastStateLoad(false), context(c)
 {
 	rek.assign(R"(\|K([0-9a-f]*(?::[0-9a-f]+)*)\|)", std::regex::ECMAScript|std::regex::optimize);
-	rem.assign(R"(\|M([\-0-9]+:[\-0-9]+:(?:[AR]:)?:[\.1][\.2][\.3][\.4][\.5])\|)", std::regex::ECMAScript|std::regex::optimize);
+	rem.assign(R"(\|M([\-0-9]+:[\-0-9]+:(?:[AR]:)?[\.1-5]{5})\|)", std::regex::ECMAScript|std::regex::optimize);
 	rec.assign(R"(\|C((?:[\-0-9]+:){6}.{15})\|)", std::regex::ECMAScript|std::regex::optimize);
 	ref.assign(R"(\|F(.{1,9})\|)", std::regex::ECMAScript|std::regex::optimize);
 }
