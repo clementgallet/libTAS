59c10dc350581630ec184033946f7f37734de05e
Clément Gallet <clement.gallet@ens-lyon.org>
2020-07-23 13:49:20 +0200

    Add errno in asserts

--- src/library/logging.h
+++ src/library/logging.h
@@ -115,7 +115,7 @@ inline void debuglog(LogCategoryFlag lcf, Args ...args)
 
 /* Macro of an assert */
 #define MYASSERT(term) if ((term)) {} \
-    else {debuglogstdio(LCF_ERROR, "%s failed in %s", #term, __func__); \
+    else {debuglogstdio(LCF_ERROR, "%s failed in %s with error %s", #term, __func__, (errno == 0)?"None":strerror(errno)); \
     exit(1);}
 
 /* We want to store and send error messages to the program so that they can be
