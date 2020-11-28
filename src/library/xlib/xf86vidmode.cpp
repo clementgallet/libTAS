98da1dc54d5ab3ca1b1a533a0615a67cfb8f0d2b
Clément Gallet <clement.gallet@ens-lyon.org>
2020-11-28 10:48:21 +0100

    Don't call xf86vmode original functions when using virtual monitor

--- src/library/xlib/xf86vidmode.cpp
+++ src/library/xlib/xf86vidmode.cpp
@@ -30,60 +30,71 @@ DEFINE_ORIG_POINTER(XF86VidModeGetAllModeLines);
 Bool XF86VidModeGetModeLine(Display* dpy, int screen, int* dotclock, XF86VidModeModeLine* modeline)
 {
     DEBUGLOGCALL(LCF_WINDOW);
-    LINK_NAMESPACE(XF86VidModeGetModeLine, "Xxf86vm");
-
-    Bool ret = orig::XF86VidModeGetModeLine(dpy, screen, dotclock, modeline);
 
     if (shared_config.screen_width) {
-        /* Change the settings. Offsets for h/vsync come from my monitor, it
+        /* Fill the settings, using my monitor as reference, it
          * shouldn't matter really. */
         modeline->hdisplay = shared_config.screen_height;
         modeline->hsyncstart = shared_config.screen_height + 88;
         modeline->hsyncend = shared_config.screen_height + 132;
         modeline->htotal = shared_config.screen_height + 280;
+        modeline->hskew = 0;
 
         modeline->vdisplay = shared_config.screen_width;
         modeline->vsyncstart = shared_config.screen_width + 4;
         modeline->vsyncend = shared_config.screen_width + 9;
         modeline->vtotal = shared_config.screen_width + 45;
+
+        modeline->flags = 5;
+        modeline->privsize = 0;
+        
+        return True;
     }
 
-    return ret;
+    LINK_NAMESPACE(XF86VidModeGetModeLine, "Xxf86vm");
+    return orig::XF86VidModeGetModeLine(dpy, screen, dotclock, modeline);        
 }
 
 Bool XF86VidModeGetAllModeLines(Display *dpy, int screen, int *modecount_return, XF86VidModeModeInfo ***modesinfo)
 {
     DEBUGLOGCALL(LCF_WINDOW);
-    LINK_NAMESPACE(XF86VidModeGetAllModeLines, "Xxf86vm");
-
-    Bool ret = orig::XF86VidModeGetAllModeLines(dpy, screen, modecount_return, modesinfo);
 
     if (shared_config.screen_width) {
-        /* Copy the first mode (current one) in another object */
-        XF86VidModeModeInfo *modeinfo = static_cast<XF86VidModeModeInfo*>(malloc(sizeof(XF86VidModeModeInfo)));
-        memcpy(modeinfo, (*modesinfo)[0], sizeof(XF86VidModeModeInfo));
+        /* If I understand correctly the doc, each individual XF86VidModeModeInfo
+         * struct are not allocated, but the array is. So we make our
+         * fake XF86VidModeModeInfo struct static, and allocate a 1-size array. */
 
-        /* Free the old result */
-        XFree((*modesinfo));
+        static XF86VidModeModeInfo modeinfo;
+        XF86VidModeModeInfo **modeinfoarr = static_cast<XF86VidModeModeInfo**>(malloc(1*sizeof(XF86VidModeModeInfo*)));
+        modeinfoarr[0] = &modeinfo;
 
-        /* Change the settings. Offsets for h/vsync come from my monitor, it
+        /* Fill the settings, using my monitor as reference, it
          * shouldn't matter really. */
-        modeinfo->hdisplay = shared_config.screen_height;
-        modeinfo->hsyncstart = shared_config.screen_height + 88;
-        modeinfo->hsyncend = shared_config.screen_height + 132;
-        modeinfo->htotal = shared_config.screen_height + 280;
+        modeinfo.dotclock = 148500;
+        
+        modeinfo.hdisplay = shared_config.screen_height;
+        modeinfo.hsyncstart = shared_config.screen_height + 88;
+        modeinfo.hsyncend = shared_config.screen_height + 132;
+        modeinfo.htotal = shared_config.screen_height + 280;
+        modeinfo.hskew = 0;
 
-        modeinfo->vdisplay = shared_config.screen_width;
-        modeinfo->vsyncstart = shared_config.screen_width + 4;
-        modeinfo->vsyncend = shared_config.screen_width + 9;
-        modeinfo->vtotal = shared_config.screen_width + 45;
+        modeinfo.vdisplay = shared_config.screen_width;
+        modeinfo.vsyncstart = shared_config.screen_width + 4;
+        modeinfo.vsyncend = shared_config.screen_width + 9;
+        modeinfo.vtotal = shared_config.screen_width + 45;
+
+        modeinfo.flags = 5;
+        modeinfo.privsize = 0;
 
         /* Set the correct returned values */
         *modecount_return = 1;
-        *modesinfo[0] = modeinfo;
+        *modesinfo = modeinfoarr;
+        
+        return True;
     }
 
-    return ret;
+    LINK_NAMESPACE(XF86VidModeGetAllModeLines, "Xxf86vm");
+    return orig::XF86VidModeGetAllModeLines(dpy, screen, modecount_return, modesinfo);
 }
 
 }
