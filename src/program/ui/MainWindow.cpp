5bafb465c2d9dbbd1eb0b74b2ced691e7eb3fd1b
jakobrs <jakobrs100@gmail.com>
2021-07-05 10:55:45 +0200

    Add LLDB support on Linux

--- src/program/ui/MainWindow.cpp
+++ src/program/ui/MainWindow.cpp
@@ -209,12 +209,32 @@ MainWindow::MainWindow(Context* c) : QMainWindow(), context(c)
 
     /* Buttons */
     launchButton = new QPushButton(tr("Start"));
-    connect(launchButton, &QAbstractButton::clicked, this, &MainWindow::slotLaunch);
+    connect(launchButton, &QAbstractButton::clicked, this, [this] { MainWindow::slotLaunch(false); });
     disabledWidgetsOnStart.append(launchButton);
 
-    launchGdbButton = new QPushButton(tr("Start and attach gdb"));
-    connect(launchGdbButton, &QAbstractButton::clicked, this, &MainWindow::slotLaunch);
-    disabledWidgetsOnStart.append(launchGdbButton);
+    launchGdbButton = new QToolButton();
+    launchGdbButton->setToolButtonStyle(Qt::ToolButtonTextOnly);
+
+    launchGdbAction = new QAction(tr("Launch with GDB"), this);
+    launchLldbAction = new QAction(tr("Launch with LLDB"), this);
+
+    connect(launchGdbAction, &QAction::triggered, this, &MainWindow::slotLaunchGdb);
+    connect(launchLldbAction, &QAction::triggered, this, &MainWindow::slotLaunchLldb);
+
+    /* launchGdbButton is a special case, it's explicitly disabled along with
+     * all the other widgets on launch
+     */
+    //disabledWidgetsOnStart.append(launchGdbButton);
+
+#ifdef __unix__
+    launchGdbButton->setPopupMode(QToolButton::MenuButtonPopup);
+
+    QMenu *launchGdbButtonMenu = new QMenu();
+    launchGdbButton->setMenu(launchGdbButtonMenu);
+
+    launchGdbButtonMenu->addAction(launchGdbAction);
+    launchGdbButtonMenu->addAction(launchLldbAction);
+#endif
 
     stopButton = new QPushButton(tr("Stop"));
     connect(stopButton, &QAbstractButton::clicked, this, &MainWindow::slotStop);
@@ -360,7 +380,7 @@ MainWindow::MainWindow(Context* c) : QMainWindow(), context(c)
     if (!context->interactive) {
         slotPause(false);
         slotFastForward(true);
-        slotLaunch();
+        slotLaunch(false);
     }
 }
 
@@ -897,6 +917,8 @@ void MainWindow::updateStatus()
             initialTimeSec->setValue(context->config.sc.initial_time_sec);
             initialTimeNsec->setValue(context->config.sc.initial_time_nsec);
 
+            launchGdbButton->setEnabled(true);
+
             if (context->config.sc.av_dumping) {
                 context->config.sc.av_dumping = false;
                 configEncodeAction->setEnabled(true);
@@ -928,6 +950,8 @@ void MainWindow::updateStatus()
                 fpsNumField->setEnabled(false);
                 fpsDenField->setEnabled(false);
             }
+
+            launchGdbButton->setEnabled(false);
 
             movieBox->setCheckable(false);
             if (context->config.sc.recording == SharedConfig::NO_RECORDING) {
@@ -1254,6 +1278,15 @@ void MainWindow::updateUIFromConfig()
 
     setRadioFromList(movieEndGroup, context->config.on_movie_end);
 
+    switch (context->config.debugger) {
+    case Config::DEBUGGER_GDB:
+        launchGdbButton->setDefaultAction(launchGdbAction);
+        break;
+    case Config::DEBUGGER_LLDB:
+        launchGdbButton->setDefaultAction(launchLldbAction);
+        break;
+    }
+
     updateStatusBar();
 }
 
@@ -1279,12 +1312,25 @@ void MainWindow::updateStatusBar()
     }
 }
 
-void MainWindow::slotLaunch()
+void MainWindow::slotLaunchGdb() {
+    context->config.debugger = Config::DEBUGGER_GDB;
+    launchGdbButton->setDefaultAction(launchGdbAction);
+
+    slotLaunch(true);
+}
+
+void MainWindow::slotLaunchLldb() {
+    context->config.debugger = Config::DEBUGGER_LLDB;
+    launchGdbButton->setDefaultAction(launchLldbAction);
+
+    slotLaunch(true);
+}
+
+void MainWindow::slotLaunch(bool attach_gdb)
 {
 
     /* Do we attach gdb ? */
-    QPushButton* button = static_cast<QPushButton*>(sender());
-    context->attach_gdb = (button == launchGdbButton);
+    context->attach_gdb = attach_gdb;
 
     if (context->status != Context::INACTIVE)
         return;
--- src/program/ui/MainWindow.h
+++ src/program/ui/MainWindow.h
@@ -31,6 +31,7 @@
 #include <QtWidgets/QCheckBox>
 #include <QtWidgets/QGroupBox>
 #include <QtWidgets/QComboBox>
+#include <QtWidgets/QToolButton>
 #include <QtCore/QElapsedTimer>
 #include <QtCore/QTimer>
 #include <forward_list>
@@ -169,7 +170,9 @@ public:
     QSpinBox *initialTimeNsec;
 
     QPushButton *launchButton;
-    QPushButton *launchGdbButton;
+    QToolButton *launchGdbButton;
+    QAction *launchGdbAction;
+    QAction *launchLldbAction;
     QPushButton *stopButton;
 
     QGroupBox *movieBox;
@@ -236,7 +239,9 @@ private slots:
     /* Update framerate values */
     void updateFramerate();
 
-    void slotLaunch();
+    void slotLaunchGdb();
+    void slotLaunchLldb();
+    void slotLaunch(bool attach_gdb);
     void slotStop();
     void slotBrowseGamePath();
     void slotGamePathChanged();
