047f2a1b12be5581b664196ffb36ed71b1d537dd
Clément Gallet <clement.gallet@ens-lyon.org>
2025-01-06 19:22:09 +0100

    Try building an .AppImage

--- build.sh
+++ build.sh
@@ -11,3 +11,13 @@ mkdir -p build
 cd build
 CXXFLAGS="-O2 -g -Wall -pedantic" ../configure "$@"
 make -j4
+
+# Build .AppImage
+
+wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy*.AppImage
+
+APPDIR_PATH=$(readlink -f ./AppDir)
+
+make prefix=/usr DESTDIR=$APPDIR_PATH install
+ARCH=x86_64 ./linuxdeploy*.AppImage --appimage-extract-and-run --appdir AppDir/ --output appimage
